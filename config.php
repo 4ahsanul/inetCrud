cfe805173f41ee185cae9e74d06e42b527fd0a8c
Yulfiz <58451478+4ahsanul@users.noreply.github.com>
2021-06-24 19:37:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
+// UNTUK KONEKSI DATABASE
 <?php
 
 $server = "localhost";
 $user = "root";
 $password = "";
-$nama_database = "db_pegawai";
+$nama_database = "db_pegawai"; // Database name yang akan digunakan
 
+// Create connection
 $db = mysqli_connect($server, $user, $password, $nama_database);
-
+// Check connection
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
